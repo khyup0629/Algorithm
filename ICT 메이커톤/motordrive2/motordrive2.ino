ad1e9c4cd8dd03d6b22b4e072fc7f100a06d408b
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-04-09 00:49:03 +0900

    Add files via upload

---
+++ ICT 메이커톤/motordrive2/motordrive2.ino
@@ -0,0 +1,82 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial btSerial(12,13);//(RX,TX)=(12,13)
+#define ENA 5 // analog
+#define IN1 6 // digital
+#define IN2 7 // digital
+#define TRIG 8 // digital
+#define ECHO 9 // digital
+
+void setup() {
+  Serial.begin(9600);
+  btSerial.begin(9600); // 소프트웨어 시리얼 통신 설정
+  pinMode(TRIG,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(ENA,OUTPUT);
+}
+
+void loop() {
+  if(btSerial.available()) //Serial.available()은 시리얼버퍼를 검사
+  {
+    char order = btSerial.read(); //Serial.read()은 시리얼버퍼에 있는 데이터를 읽어들임
+    switch(order)
+    {
+      case 'o': //o인경우 문열기
+      digitalWrite(IN1,HIGH);
+      digitalWrite(IN2,LOW);
+      delay(600); // door open time 고치기
+      digitalWrite(IN1,LOW);
+      digitalWrite(IN2,LOW);
+      break;
+      case 'c': //c인경우 문닫기
+      digitalWrite(IN1,LOW);
+      digitalWrite(IN2,HIGH);
+      delay(600); // door close time 고치기
+      digitalWrite(IN1,LOW);
+      digitalWrite(IN2,LOW);
+      break;
+      default:
+      digitalWrite(IN1,LOW);
+      digitalWrite(IN2,LOW);
+    }
+  }
+    digitalWrite(TRIG,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG,LOW);
+    int dur = pulseIn(ECHO,HIGH);
+    float dis = (float) dur * 0.170;
+    Serial.print(dis);
+    Serial.println("mm");
+  
+    if (dis <= 300){ // drone come
+      // door open
+      digitalWrite(IN1,HIGH);
+      digitalWrite(IN2,LOW); // L9110S
+      delay(600); // door open time 고치기
+      while (1){
+        digitalWrite(TRIG,HIGH);
+        delayMicroseconds(10);
+        digitalWrite(TRIG,LOW);
+        int dur = pulseIn(ECHO,HIGH);
+        float dis = (float) dur * 0.170;
+        Serial.print(dis);
+        Serial.println("mm");
+        if (dis <= 300){ // still inner 200m
+          digitalWrite(IN1,LOW);
+          digitalWrite(IN2,LOW); // door stop
+        }
+        else{ // drone gone
+          // door close
+          digitalWrite(IN1,LOW);
+          digitalWrite(IN2,HIGH);
+          delay(600); // door close time 고치기
+          digitalWrite(IN1,LOW);
+          digitalWrite(IN2,LOW); // door stop
+          break; // out of while{}
+        }
+        delay(4000); // 4초마다 거리 측정
+      }
+    }
+    delay(4000); //타협
+}
