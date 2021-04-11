c97762b96d281d9b3d8fb6c10e7299129a0c1d74
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-04-11 18:17:20 +0900

    Update BFS 동작 방식 예시.py

--- BFS/BFS 동작 방식 예시.py
+++ BFS/BFS 동작 방식 예시.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+
 def bfs(graph, start, visited):
     # 큐 구현을 위해 deque 라이브러리 사용
     queue = deque([start])
@@ -13,6 +14,7 @@ def bfs(graph, start, visited):
                 queue.append(i)
                 visited[i] = True
 
+                
 # 각 노드가 연결된 정보를 표현 (2차원 리스트)
 # index와 노드 번호를 효과적으로 연결짓기 위해 일부러 0번째는 비워둔다
 graph = [[], [2, 3, 8], [1, 7], [1, 4, 5], [3, 5], [3, 4], [7], [2, 6, 8], [1, 7]]
