43245ae193f3036c1f4be1c409d76538f326072c
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-04-19 21:39:42 +0900

    Add files via upload

---
+++ 자료구조/우선순위 큐 힙 정렬 구현.py
@@ -0,0 +1,32 @@
+import sys
+# 힙 정렬과 우선순위 큐를 위한 라이브러리 heapq
+# 파이썬에서는 min_heapify() 로 동작
+import heapq
+input = sys.stdin.readline
+
+
+def heapsort(iterable):
+    h = []
+    result = []
+    # 힙 정렬 수행
+    # heapq.heappush(리스트, 값) : '리스트' 에 '값'을 힙 정렬로 삽입
+    for value in iterable:
+        heapq.heappush(h, value)
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    # heapq.heappop(리스트) : '리스트' 의 맨 앞의 원소를 뽑아냄.
+    for _ in range(len(h)):
+        result.append(heapq.heappop(h))
+    return result
+
+
+# 입력값의 개수
+n = int(input())
+array = []
+# 입력 받은 숫자를 array 에 차례대로 기록
+for _ in range(n):
+    array.append(int(input()))
+# 힙 정렬 수행한 후 정렬된 리스트를 res 에 저장
+res = heapsort(array)
+
+for i in res:
+    print(i)
