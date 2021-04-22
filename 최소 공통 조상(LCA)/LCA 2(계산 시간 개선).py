2a6db994c7c50ed96ac4f360a7767b997f1111c3
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-04-22 14:59:10 +0900

    Update LCA 2(계산 시간 개선).py

--- 최소 공통 조상(LCA)/LCA 2(계산 시간 개선).py
+++ 최소 공통 조상(LCA)/LCA 2(계산 시간 개선).py
@@ -9,7 +9,7 @@ LOG = 21  # 2^20 = 1,000,000
 n = int(input())
 parent = [[0] * LOG for _ in range(n + 1)]
 depth = [0] * (n + 1)
-visited = [0] * (n + 1)
+visited = [False] * (n + 1)
 
 graph = [[] for _ in range(n+1)]
 for _ in range(n-1):
@@ -22,10 +22,10 @@ def dfs(x, d):
     visited[x] = True
     depth[x] = d
     for i in graph[x]:
-        if visited[i]:
-            continue
-        parent[i][0] = x
-        dfs(i, d + 1)
+        if not visited[i]:
+            visited[i] = True
+            parent[i][0] = x
+            dfs(i, d + 1)
 
 
 def set_parent():  # 2^i 만큼 거슬러 올라갔을 때의 부모 노드 번호를 기록
@@ -60,3 +60,48 @@ m = int(input())
 for i in range(m):
     a, b = map(int, input().split())
     print(lca(a, b))
+
+"""
+문제
+N(2 ≤ N ≤ 100,000)개의 정점으로 이루어진 트리가 주어진다. 트리의 각 정점은 1번부터 N번까지 번호가 매겨져 있으며, 루트는 1번이다.
+
+두 노드의 쌍 M(1 ≤ M ≤ 100,000)개가 주어졌을 때, 두 노드의 가장 가까운 공통 조상이 몇 번인지 출력한다.
+
+입력
+첫째 줄에 노드의 개수 N이 주어지고, 다음 N-1개 줄에는 트리 상에서 연결된 두 정점이 주어진다. 
+그 다음 줄에는 가장 가까운 공통 조상을 알고싶은 쌍의 개수 M이 주어지고, 다음 M개 줄에는 정점 쌍이 주어진다.
+
+출력
+M개의 줄에 차례대로 입력받은 두 정점의 가장 가까운 공통 조상을 출력한다.
+
+예제 입력 1 
+15
+1 2
+1 3
+2 4
+3 7
+6 2
+3 8
+4 9
+2 5
+5 11
+7 13
+10 4
+11 15
+12 5
+14 7
+6
+6 11
+10 9
+2 6
+7 6
+8 13
+8 15
+예제 출력 1 
+2
+4
+2
+1
+3
+1
+"""
