640d098e53e1e279f31cbcc45cdd91a33c7221a8
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-05-06 15:30:18 +0900

    Update 완주하지 못한 선수.py

--- 해싱(Hashing)/완주하지 못한 선수.py
+++ 해싱(Hashing)/완주하지 못한 선수.py
@@ -1,5 +1,25 @@
-# a.keys() 에 대해서 알 수 있었던 문제
+# 계산 시간이 더 빠른 코드
+# 밑에 코드와 비교하면, 이 코드는 participant에 있는 사람이
+# completion에 없다면 바로 즉시 값을 반환하고 코드를 종료한다.
+def solution(participant, completion):
+    answer = ''
+    hash_map = {}
+    # 같은 키(key)가 들어가면 덮어씌워진다. 그래서 value를 개수로 했다.
+    for person in completion:
+        hash_map[person] = hash_map.get(person, 0) + 1
 
+    for person in participant:
+        if person in hash_map:
+            hash_map[person] -= 1
+            if hash_map[person] == 0:
+                del hash_map[person]
+        else:
+            return person
+
+    return answer
+
+
+# a.keys() 에 대해서 알 수 있었던 문제
 def solution(participant, completion):
     dic = {}
     for person in participant:
