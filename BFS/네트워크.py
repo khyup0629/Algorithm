0dd516c47be97c3acfa7abb610e2e7a9d0d4c793
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-05-07 15:09:04 +0900

    Add files via upload

---
+++ BFS/네트워크.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+def solution(n, computers):
+    answer = 0
+
+    visited = [False] * n  # 방문 체크
+
+    graph = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(i + 1, n):
+            if computers[i][j] == 1:
+                graph[i].append(j)
+                graph[j].append(i)
+
+    def bfs(start):
+        q = deque()
+        q.append(start)
+        visited[start] = True
+        while q:
+            now = q.popleft()
+            for x in graph[now]:
+                if not visited[x]:
+                    visited[x] = True
+                    q.append(x)
+
+    for i in range(n):
+        if not visited[i]:
+            bfs(i)
+            answer += 1
+
+    return answer
+
+# 문제 : https://programmers.co.kr/learn/courses/30/lessons/43162#
