698b21dff5fc2e0fa33316302716999761812b17
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-06-01 14:02:12 +0900

    Add files via upload

---
+++ 다이나믹 프로그래밍/피보나치 수 5.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+F = [0] * (n+1)
+if n >= 1:
+    F[1] = 1
+    for i in range(2, n+1):
+        F[i] = F[i-1] + F[i-2]
+
+print(F[n])
+
+# 문제 : https://www.acmicpc.net/problem/10870
