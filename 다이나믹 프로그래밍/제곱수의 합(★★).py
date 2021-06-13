d51f4bbd96b8483fac2f1bdc10f00f3d818cda80
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-06-13 22:40:17 +0900

    Update and rename 제곱수의 합.py to 제곱수의 합(★★).py

--- 다이나믹 프로그래밍/제곱수의 합(★★).py
+++ 다이나믹 프로그래밍/제곱수의 합(★★).py
@@ -4,12 +4,13 @@
 from math import *
 n = int(input())
 
-dp = [i for i in range(n+1)]
+dp = [i for i in range(n+1)]  # 최대 경우는 1의 제곱으로만 이루어진 경우로 한다.
 
 for i in range(1, n+1):
-    standard = int(sqrt(i))
+    standard = int(sqrt(i))  # i보다 작거나 같은 최대 제곱수를 찾는다.
+    # 41의 경우 5^2 + 4^2 가 가장 최소 개수이다.
     for j in range(1, standard+1):
-        if dp[i] > dp[i-j**2] + 1:
+        if dp[i] > dp[i-j**2] + 1:  # 제곱수의 개수가 가장 최소가 되는 값을 찾는다.
             dp[i] = dp[i-j**2] + 1
 
 print(dp[n])
