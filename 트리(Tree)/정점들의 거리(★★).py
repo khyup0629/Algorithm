efa933649ba5e419055aae8d48d0a87813c38618
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-08-11 21:25:18 +0900

    Update 정점들의 거리(★★).py

--- 트리(Tree)/정점들의 거리(★★).py
+++ 트리(Tree)/정점들의 거리(★★).py
@@ -4,54 +4,61 @@
 # 3. 임의로 루트 노드는 1로 해서 깊이와 부모 노드 테이블을 기록한다.
 import sys
 sys.setrecursionlimit(10**5)
-input = sys.stdin.readline
 
 n = int(input())
 
+# 그래프에 연결되는 노드와 거리 정보를 같이 넣습니다.
 graph = [[] for _ in range(n+1)]
 for _ in range(n-1):
-    start, end, cost = map(int, input().split())
-    graph[start].append((end, cost))
-    graph[end].append((start, cost))
+    node1, node2, cost = map(int, input().split())
+    graph[node1].append([node2, cost])
+    graph[node2].append([node1, cost])
 
 depth = [0] * (n + 1)
 parent = [0] * (n + 1)
 visited = [False] * (n + 1)
 
 
-def dfs(x, d):
+def findDepthParent(x, d):  # 각 노드의 부모노드와 거리, 깊이를 계산합니다.
     visited[x] = True
     depth[x] = d
     for i, cost in graph[x]:
         if not visited[i]:
-            parent[i] = (x, cost)  # 올라갈 때의 간선의 비용도 함께 기록
-            dfs(i, d+1)
+            parent[i] = (x, cost)
+            findDepthParent(i, d+1)
 
 
 def lca(a, b):
-    global result
-    if depth[a] > depth[b]:  # 항상 b의 깊이가 더 깊도록
+    dist = 0  # 두 노드의 거리
+    # 두 매개변수 중 항상 b의 깊이가 더 깊도록 설정합니다.
+    if depth[a] > depth[b]:
         a, b = b, a
-    while depth[a] != depth[b]:  # 둘의 깊이가 같아질 때까지
-        result += parent[b][1]
+    # 두 노드의 깊이가 같아질 때까지 거슬러 올라갑니다.
+    while depth[a] != depth[b]:
+        dist += parent[b][1]
         b = parent[b][0]
+    # 같은 깊이에서 두 노드가 같아진다면 함수를 종료합니다.
+    # 이 과정을 넣지 않으면 밑에 while문에서 인덱스 에러가 발생합니다.
     if a == b:
-        return
-    # 같이 올라가기
+        return dist
+    # 두 노드의 부모노드가 같아질 때까지 거슬러 올라갑니다.
+    # 반복 종료 시점은 부모노드가 같을 때입니다.
+    # 부모노드까지 거슬러올라가서 두 노드가 같아질 때까지가 아닙니다.
     while parent[a][0] != parent[b][0]:
-        result += parent[a][1] + parent[b][1]
+        dist += (parent[a][1] + parent[b][1])
         a = parent[a][0]
         b = parent[b][0]
-    result += parent[a][1] + parent[b][1]
+    # 부모 노드가 같으면 거슬러 올라가지 않고 반복을 종료했기 때문에 마지막 cost까지 더해주어야합니다.
+    dist += (parent[a][1] + parent[b][1])
+    return dist
 
+
+findDepthParent(1, 0)  # 루트 노드를 항상 1로 놓고, 깊이는 0부터 시작합니다.
 
 m = int(input())
-dfs(1, 0)  # 임의로 루트 노드는 1
 for _ in range(m):
-    a, b = map(int, input().split())
-    result = 0
-    lca(a, b)
-    print(result)
+    node1, node2 = map(int, input().split())
+    print(lca(node1, node2))
 
 
 # 기존 DFS로 풀면 시간 초과(PyPy 제출 시 메모리 초과)
