be7dbba1667e0aaa96df00f54b67c9b43af19ccd
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-08-11 15:33:05 +0900

    Update 수 찾기.py

--- 이진 탐색 (Binary search)/수 찾기.py
+++ 이진 탐색 (Binary search)/수 찾기.py
@@ -4,50 +4,22 @@
 # 개수를 탐색하게 되면 계산 과정을 확연히 줄일 수 있다.
 
 from bisect import bisect_left, bisect_right
-import sys
-input = sys.stdin.readline  # 입력을 빠르게 받아서 시간 단축
-
 n = int(input())
-
-array = list(map(int, input().split()))
-
+arrFirst = list(map(int, input().split()))
 m = int(input())
+arrSecond = list(map(int, input().split()))
 
-count = list(map(int, input().split()))
-array.sort()  # 정렬된 리스트에 대해서 bisect 를 써야하므로
+arrFirst.sort()
 
-for i in count:
-    start = bisect_left(array, i)
-    end = bisect_right(array, i)
-    if (end - start) > 0:
+for i in range(m):
+    idxLeft = bisect_left(arrFirst, arrSecond[i])
+    idxRight = bisect_right(arrFirst, arrSecond[i])
+    if idxRight - idxLeft > 0:
         print(1)
     else:
         print(0)
 
-"""
-문제
-N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다. 
-다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다. 다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면 된다.
-모든 정수의 범위는 -231 보다 크거나 같고 231보다 작다.
-
-출력
-M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
-
-예제 입력 1 
-5
-4 1 5 2 3
-5
-1 3 7 9 5
-예제 출력 1 
-1
-1
-0
-0
-1
-"""
+# 문제 : https://www.acmicpc.net/problem/1920        
 
 """
 # (count 함수를 사용해 찾게 되면 시간 초과)
