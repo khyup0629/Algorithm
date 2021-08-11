0bf13f4186faeaf58365c2abeea51a77e75c3102
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-08-11 14:28:51 +0900

    Update 나무 자르기.py

--- 이진 탐색 (Binary search)/나무 자르기.py
+++ 이진 탐색 (Binary search)/나무 자르기.py
@@ -2,29 +2,25 @@
 # 이진 탐색을 수행해서 알고리즘 코딩
 # PyPy3 제출
 
-import sys
-input = sys.stdin.readline
-
-# 나무의 수, 집으로 가져가려고 하는 나무의 길이
 n, m = map(int, input().split())
-# 나무의 높이
-array = list(map(int, input().split()))
-# 나무의 시작점과 끝점
-start = 0
-end = max(array)
+woodLen = list(map(int, input().split()))
 
-# 이진 탐색 수행
-result = 0
+start, end = 0, max(woodLen)
+ans = 0  # 정답
 while start <= end:
-    total = 0
-    mid = (start + end) // 2  # 소숫점 이하 자리 버림
-    for i in array:
-        if i > mid:  # 기준을 넘어가는 것만 자른다
-            total += (i - mid)
-    if total >= m:
-        result = mid
+    mid = (start + end) // 2
+
+    takeWood = 0  # 가져갈 나무의 총 길이
+    for wood in woodLen:
+        if wood > mid:  # 기준을 넘어가는 것만 자릅니다.
+            takeWood += (wood - mid)
+
+    if takeWood >= m:
+        ans = mid  # 적어도 m개를 가져가야 하므로 m개 이상을 가져가면 답을 갱신합니다.
         start = mid + 1
     else:
         end = mid - 1
 
-print(result)
+print(ans)
+
+# 문제 : https://www.acmicpc.net/problem/2805
