6634c5825209128d741aa24c58813aecc04c52ac
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-08-12 13:40:01 +0900

    Update Cubeditor(★★).py

--- KMP/Cubeditor(★★).py
+++ KMP/Cubeditor(★★).py
@@ -1,25 +1,22 @@
 # 해싱으로 풀 경우 메모리 초과현상이 나타난다.
 # 따라서, KMP 알고리즘을 공부해서 이용했다.
 # 부분 문자열 : 부분 수열과는 다르게 투 포인터처럼 연속된 문자열이다.
-string_i = input()
-l = len(string_i)
+string = input()
 
-array = []
-for k in range(l):
-    string = string_i[k:l]
-    pi = [0] * len(string)
+maxLen = 0
+for k in range(len(string)):
+    s = string[k:]
+    n = len(s)
+    pi = [0] * n
     j = 0
-    for i in range(1, len(string)):
-        # 이 부분이 이해가 안된다.
-        while string[j] != string[i] and j != 0:
+    for i in range(1, n):
+        while j > 0 and s[i] != s[j]:
             j = pi[j-1]
-        if string[j] == string[i]:
+        if s[i] == s[j]:
             j += 1
             pi[i] = j
-    print(pi)
-    array.append(max(pi))
+    maxLen = max(maxLen, max(pi))
 
-print(max(array))
+print(maxLen)
 
 # 문제 : https://www.acmicpc.net/problem/1701
-
