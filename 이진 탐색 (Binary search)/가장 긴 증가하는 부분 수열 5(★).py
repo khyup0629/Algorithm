4c85bada1fe9b0add7f501e0f84e84364bc9fe6d
khyup0629 <43658658+khyup0629@users.noreply.github.com>
2021-08-25 17:25:01 +0900

    Update 가장 긴 증가하는 부분 수열 5(★).py

--- 이진 탐색 (Binary search)/가장 긴 증가하는 부분 수열 5(★).py
+++ 이진 탐색 (Binary search)/가장 긴 증가하는 부분 수열 5(★).py
@@ -1,36 +1,39 @@
-# '가장 긴 증가하는 부분 수열 3'의 확장 문제.
-# 수열에 속해있는 어떤 수의 인덱스와 대응하도록 dp 테이블을 만든다.
-# dp 테이블에는 어떤 수가 가질 수 있는 최대 길이가 저장된다.
-from bisect import bisect_left
+# '가장 긴 증가하는 부분 수열 3'의 확장 문제입니다.
+# 문제 풀이 아이디어
+# 1. 가장 긴 증가하는 부분 수열의 최대 길이를 구합니다.(bisect를 이용해 시간 복잡도를 개선합니다)
+# 2. 구하면서 수열의 인덱스와 대응되는 리스트를 하나 만들고 어떤 수가 가질 수 있는 최대 길이를 저장합니다.
+# 3. 수열을 뒤에서부터 살펴보면서 i번째 수의 최대 길이(indexInfo에 저장되어 있습니다)가 가장 긴 증가하는 부분 수열의 길이라면,
+# 결괏값 리스트에 추가하고 가장 긴 증가하는 부분 수열의 길이를 -1하면서 수열의 처음까지 탐색합니다.
+# 4. 결괏값 리스트는 내림차순으로 저장되어 있으므로, 오름차순으로 뒤집어준다음 출력합니다.
 
+from bisect import bisect_left
 n = int(input())
 arr = list(map(int, input().split()))
-dp = [1] * n  # 어떤 수가 가질 수 있는 최대 길이가 저장된다.
+indexInfo = [0] * n  # 어떤 수가 가질 수 있는 최대 길이가 저장됩니다.
+indexInfo[0] = 1
 
 # temp에 속해있는 원소들로는 가장 긴 증가하는 부분 수열이라 할 수 없는 임시 수열.
 temp = [arr[0]]
 for i in range(1, n):
-    if temp[-1] < arr[i]:
+    idx = bisect_left(temp, arr[i])
+    indexInfo[i] = idx + 1  # i번째 수가 가질 수 있는 최대 길이
+    if arr[i] > temp[-1]:
         temp.append(arr[i])
-        dp[i] = len(temp)
     else:
-        idx = bisect_left(temp, arr[i])
         temp[idx] = arr[i]
-        dp[i] = idx + 1
 
-maxLen = max(dp)  # 가장 증가하는 부분 수열의 길이
+maxLen = len(temp)  # 가장 증가하는 부분 수열의 길이
 print(maxLen)
 
-resultArr = []  # 수가 내림차순으로 저장된다.
-for i in range(n-1, -1, -1):  # DP 테이블의 뒤에서부터 확인하면서
-    if dp[i] == maxLen:  # 최대 길이와 dp 테이블의 해당하는 인덱스가 같다면,
-        resultArr.append(arr[i])  # array 중 해당하는 인덱스의 수를 저장한다.
+temp = []  # 수가 내림차순으로 저장됩니다.
+for i in range(n-1, -1, -1):  # indexInfo 테이블의 뒤에서부터 확인하면서
+    if indexInfo[i] == maxLen:  # 최대 길이와 indexInfo 테이블의 해당하는 인덱스가 같다면,
+        temp.append(arr[i])  # array 중 해당하는 인덱스의 수를 저장합니다.
         maxLen -= 1
 
-# 내림차순을 뒤집어 오름차순으로 만든 다음 출력한다.
-resultArr = resultArr[::-1]
-for num in resultArr:
-    print(num, end=' ')
+# 내림차순을 뒤집어 오름차순으로 만든 다음 출력합니다.
+temp = temp[::-1]
+print(*temp)
 
 # 문제 : https://www.acmicpc.net/problem/14003
 
